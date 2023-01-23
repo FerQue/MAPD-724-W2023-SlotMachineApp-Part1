f84d3821dd41b0bd0213175a74c1c6130dce689b
FerQue <fernando_quezada1785@outlook.com>
2023-01-22 19:02:08 -0500

    First Commit
    i have created the project and updated it.

---
+++ SlotMachineApp1/SlotMachineApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlotMachineApp1
+//
+//  Created by Victor Quezada on 2023-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
