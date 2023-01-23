3fc06b37e4c9177017c17c5859e739c29cc410c8
FerQue <fernando_quezada1785@outlook.com>
2023-01-22 21:10:37 -0500

    Last update
    We have added some personal information.

--- SlotMachineApp1/SlotMachineApp1/ViewController.swift
+++ SlotMachineApp1/SlotMachineApp1/ViewController.swift
@@ -2,8 +2,12 @@
 //  ViewController.swift
 //  SlotMachineApp1
 //
-//  Created by Victor Quezada on 2023-01-22.
-//
+//  MAPD724-W2023 Advanced iOS Development
+//  Victor Quezada - 301286477
+//  Sankjay Nithyanandalingam - 301296000
+//  Sunday January 22, 2023
+//  We have to create a slot machine app - part 1 User Interface (UI). No functional logic is required.
+//  We have updated in GitHub as MAPD-724-W2023-SlotMachineApp-Part1
 
 import UIKit
 
